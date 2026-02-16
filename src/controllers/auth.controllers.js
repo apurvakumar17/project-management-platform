672c178a5670de4300125d4152065db2577b8ebb
Apurva Kumar <apurva7k@gmail.com>
2026-02-17 00:01:54 +0530

    Login functionality added
    - Installed cookie-parser module to access clients cookies
    - "/login" route added
    - Login controller added
    - Login data validators added
    - Login functionality testing using Postman Testing software

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -70,6 +70,49 @@ const registerUser = asyncHandler(async (req, res) => {
             "User Registered!"
         ),
     )
-})
+});
 
-export {registerUser};
+const login = asyncHandler(async (req, res) => {
+    const {email, password, username} = req.body
+
+    if (!username && !email) {
+        throw new ApiError (400, "Username & email is required!");
+    }
+    const user = await User.findOne({email})
+
+    if (!user) {
+        throw new ApiError(400, "User does not exist! ");
+    }
+    const isPasswordValid = user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(400, "Invalid Credentials! ");
+    }
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+    const options = {
+        httpsOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User logged in successfully! "
+            )
+        )
+});
+
+export {registerUser, login};
